0c6e6cc17f0af431b559cf30a68daec5c8c79a1e
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-04-12 10:52:30 -0700

    Augmenting data

--- wisconsin/augment.py
+++ wisconsin/augment.py
@@ -11,26 +11,39 @@ def fetch_clerk_results(address):
     zipcode = parts[2].split('WI ')[1].strip()
     data = {'addressLine': parts[0].strip(), 'unit': "", 'city': parts[1].strip(), 'zip': zipcode}
   if len(parts) == 2:
-    city, zipcode = parts[1].split(' WI ')
+    try:
+      city, zipcode = parts[1].split(' WI ')
+    except ValueError:
+      city, zipcode = parts[1].split(' WI. ')  # some mistakes in the file
     data = {'addressLine': parts[0].strip(), 'unit': "", 'city': city, 'zip': zipcode}
 
   result = requests.post(URL, data=data)
   return result.json()
 
 
+def save_data(address, file_name):
+  if os.path.isfile(file_name):
+    return
+  data = fetch_clerk_results(address)
+  with open(file_name, 'w') as fh:
+    json.dump(data, fh)
+
 if __name__ == '__main__':
   with open('results/records.noemail.json') as fh:
     records = json.load(fh)
-    print(len(records))
-    [r['municipal_address'] for r in records if len(r['municipal_address'].split(',')) != 3]
 
   for record in tqdm(records[:None]):
     key = record['key']
-    file_name = f'results/municipal_address/{key}.json'
-    if os.path.isfile(file_name):
-      continue
-    data = fetch_clerk_results(record['municipal_address'])
-    with open(file_name, 'w') as fh:
-      json.dump(data, fh)
+    if record['municipal_address']:
+      save_data(
+        record['municipal_address'],
+        f'results/municipal_address/{key}.json',
+      )
+
+    if record['mailing_address']:
+      save_data(
+        record['mailing_address'],
+        f'results/mailing_address/{key}.json',
+      )
 
     # Really care about ['Data']['clerk']
