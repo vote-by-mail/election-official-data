7e515cf4c1d7cdb0bcfc8e0b2e346b5f5b16b944
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-04-11 17:24:36 -0700

    editorconfig

--- georgia/crawl.py
+++ georgia/crawl.py
@@ -4,47 +4,47 @@ import random
 import json
 
 def goto_county(i):
-    element = driver.find_element_by_xpath("//select[@name='idTown']")
-    all_options = element.find_elements_by_tag_name("option")
-    if i >= len(all_options):
-        return False
-    all_options[i].click()
-    driver.find_element_by_xpath("//input[@name='SubmitCounty']").click()
-    return True
+  element = driver.find_element_by_xpath("//select[@name='idTown']")
+  all_options = element.find_elements_by_tag_name("option")
+  if i >= len(all_options):
+    return False
+  all_options[i].click()
+  driver.find_element_by_xpath("//input[@name='SubmitCounty']").click()
+  return True
 
 def scrape_county_html():
-    el = driver.find_element_by_class_name("elections")
-    return el.get_attribute('outerHTML')
+  el = driver.find_element_by_class_name("elections")
+  return el.get_attribute('outerHTML')
 
 def go_back():
-    driver.find_element_by_xpath("//input[@name='SubmitBack']").click()
+  driver.find_element_by_xpath("//input[@name='SubmitBack']").click()
 
 def save_html(i, html):
-    with open(f'results/result.{i}.html', 'w') as fh:
-        fh.write(html)
+  with open(f'results/result.{i}.html', 'w') as fh:
+    fh.write(html)
 
 
 if __name__ == '__main__':
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    driver = webdriver.Chrome(options=options)
+  options = webdriver.ChromeOptions()
+  options.add_argument('headless')
+  driver = webdriver.Chrome(options=options)
 
-    driver.get("https://elections.sos.ga.gov/Elections/countyregistrars.do")
+  driver.get("https://elections.sos.ga.gov/Elections/countyregistrars.do")
 
-    i = 0
-    htmls = []
-    while True:
-        print(f"Fetching County {i}")
-        success = goto_county(i)
-        if not success:
-            break
+  i = 0
+  htmls = []
+  while True:
+    print(f"Fetching County {i}")
+    success = goto_county(i)
+    if not success:
+      break
 
-        wait = random.uniform(1, 3)
-        time.sleep(wait)
+    wait = random.uniform(1, 3)
+    time.sleep(wait)
 
-        html = scrape_county_html()
-        save_html(i, html)
-        go_back()
-        i += 1
+    html = scrape_county_html()
+    save_html(i, html)
+    go_back()
+    i += 1
 
-    driver.close()
+  driver.close()
--- georgia/parse.py
+++ georgia/parse.py
@@ -6,61 +6,61 @@ from bs4.element import NavigableString
 files = glob('results/result.*.html')
 
 def parse_addr_line(key, line):
-    if not isinstance(line, NavigableString):
-        return {}
-    else:
-        return {key: line.strip()}
+  if not isinstance(line, NavigableString):
+    return {}
+  else:
+    return {key: line.strip()}
 
 def parse_contact_line(line):
-    if not isinstance(line, NavigableString):
-        return {}
-    parsed = line.split(':')
-    if len(parsed) == 2:
-        k, v = parsed
-        return {k.strip(): v.strip()}
+  if not isinstance(line, NavigableString):
     return {}
+  parsed = line.split(':')
+  if len(parsed) == 2:
+    k, v = parsed
+    return {k.strip(): v.strip()}
+  return {}
 
 def parse_contact(h4):
-    title = h4.text
-    line1 = h4.next_element.next_element
-    line2 = line1.next_element.next_element
-    if title.endswith('Address:'):
-        return {
-            'kind': title,
-            **parse_addr_line('addr1', line1),
-            **parse_addr_line('addr2', line2),
-        }
-    elif title == 'Contact Information:':
-        return {
-            'kind': title,
-            **parse_contact_line(line1),
-            **parse_contact_line(line2),
-        }
-    else:
-        raise ValueError('Encountered unrecognized contact')
+  title = h4.text
+  line1 = h4.next_element.next_element
+  line2 = line1.next_element.next_element
+  if title.endswith('Address:'):
+    return {
+      'kind': title,
+      **parse_addr_line('addr1', line1),
+      **parse_addr_line('addr2', line2),
+    }
+  elif title == 'Contact Information:':
+    return {
+      'kind': title,
+      **parse_contact_line(line1),
+      **parse_contact_line(line2),
+    }
+  else:
+    raise ValueError('Encountered unrecognized contact')
 
 def parse_html(file):
-    with open(file) as fh:
-        soup = BeautifulSoup(fh.read(), "lxml")
+  with open(file) as fh:
+    soup = BeautifulSoup(fh.read(), "lxml")
 
-    county = soup.find('hr').next_element
-    name = county.next_element.next_element
+  county = soup.find('hr').next_element
+  name = county.next_element.next_element
 
-    contact_els = soup.find_all('h4')
-    contacts = [parse_contact(contact) for contact in contact_els]
+  contact_els = soup.find_all('h4')
+  contacts = [parse_contact(contact) for contact in contact_els]
 
-    email_el = soup.find('a')
-    email = email_el.text.strip() if email_el else None
+  email_el = soup.find('a')
+  email = email_el.text.strip() if email_el else None
 
-    return {
-        'county': county.strip(),
-        'name': name.strip(),
-        'contacts': contacts,
-        'email': email,
-    }
+  return {
+    'county': county.strip(),
+    'name': name.strip(),
+    'contacts': contacts,
+    'email': email,
+  }
 
 if __name__ == '__main__':
-    js = [parse_html(file) for file in files]
+  js = [parse_html(file) for file in files]
 
-    with open('results/result.json', 'w') as fh:
-        json.dump(js, fh)
+  with open('results/result.json', 'w') as fh:
+    json.dump(js, fh)
