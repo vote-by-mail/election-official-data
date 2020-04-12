894812b038551a8db1c43de00d849e5510317fba
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-04-11 21:49:06 -0700

    parsing wisconsin PDFs

--- wisconsin/parse.py
+++ wisconsin/parse.py
@@ -1,5 +1,7 @@
 import PyPDF2
 import re
+import json
+from tqdm import tqdm
 
 city_start_re = re.compile('^(CITY|TOWN|VILLAGE) OF')
 end_page_re = re.compile('^Page \d+ of 360$')
@@ -25,37 +27,45 @@ def chunk_city(text):
     if within_city:
       city_lines += [line]
 
-city_county_re = re.compile(r'(CITY|TOWN|VILLAGE)\s+OF\s+([A-Z \n]+)\s+-\s+([A-Z \n]+)\s+COUNTY')
+city_county_re = re.compile(r'(CITY|TOWN|VILLAGE)\s+OF\s+([A-Z.\- \n]+)\s+-\s+([A-Z.\- \n]+\s+COUNTY|MULTIPLE\s+COUNTIES)')
 clerk_re = re.compile('CLERK: (.*)')
+deputy_clerk_re = re.compile('DEPUTY CLERK: (.*)')
 # address_re = re.compile(r'Municipal Address :([A-Z0-9, \n]+\s+\d{5}(-\d{4})?)')
 municipal_address_re = re.compile(r'Municipal Address :([^:]+\n)+')
 mailing_address_re = re.compile(r'Mailing Address :([^:]+\n)+')
 phone_re = re.compile(r'Phone \d: ([()\d-]+)')
 fax_re = re.compile(r'Fax: ([()\d-]+)')
+url_re = re.compile('(https?://[^\s/$.?#].[^\s]*)')
+
+def first_group(regex, lines):
+  match = regex.search(lines)
+  return match.group(1).strip() if match else None
 
 def parse_city_lines(lines):
   match = city_county_re.search(lines)
-  clerk = clerk_re.search(lines).group(1)
-  municipal_address = municipal_address_re.search(lines)
-  mailing_address = mailing_address_re.search(lines)
-  phones = phone_re.findall(lines)
-  fax = fax_re.search(lines)
+
   return {
-    'type': match.group(1),
+    'city_type': match.group(1),
     'city': match.group(2),
     'county': match.group(3),
-    'clerk': clerk,
-    'municipal_address': municipal_address.group(1) if municipal_address else None,
-    'mailing_address': mailing_address.group(1) if mailing_address else None,
-    'phones': phones,
-    'fax': fax.group(1) if fax else None,
+    'clerk': first_group(clerk_re, lines),
+    'deputy_clerk': first_group(deputy_clerk_re, lines),
+    'municipal_address': first_group(municipal_address_re, lines),
+    'mailing_address': first_group(mailing_address_re, lines),
+    'phones': phone_re.findall(lines),
+    'fax': first_group(fax_re, lines),
+    'url': first_group(url_re, lines),
   }
 
-with open('results/WI Municipal Clerks no emails Updated 3-23-2020.pdf', 'rb') as fh:
-  pdf_reader = PyPDF2.PdfFileReader(fh)
-  # full_text = [pdf_reader.getPage(page).extractText() for page in range(pdf_reader.numPages)]
-  text = pdf_reader.getPage(3).extractText()
-  for city_lines in chunk_city(text):
-    # print(city_lines)
-    print(parse_city_lines(city_lines))
+if __name__ == '__main__':
+  with open('results/WI Municipal Clerks no emails Updated 3-23-2020.pdf', 'rb') as fh:
+    pdf_reader = PyPDF2.PdfFileReader(fh)
+    # full_text = [pdf_reader.getPage(page).extractText() for page in range(pdf_reader.numPages)]
+    records = []
+    for page_num in tqdm(range(pdf_reader.numPages)):
+      text = pdf_reader.getPage(page_num).extractText()
+      for city_lines in chunk_city(text):
+        records += [parse_city_lines(city_lines)]
 
+  with open('results/records.json', 'w') as fh:
+    json.dump(records, fh)
