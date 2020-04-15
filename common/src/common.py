ac110207623445113cb14d2c18bf2922a99349c6
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-04-15 14:21:24 -0700

    Adding virginia

--- common/src/common.py
+++ common/src/common.py
@@ -1,11 +1,16 @@
 import requests
+import time
 from ediblepickle import checkpoint
 
 def key_namer(args, kwargs):
-  url = args[0]
-  return str(abs(hash(url))) + '.pkl'
+  kwargs.pop('wait', None)
+  return str(abs(hash(str(args) + str(kwargs)))) + '.pkl'
 
 @checkpoint(key=key_namer, work_dir='cache')
-def cache_request(url):
-  response = requests.get(url)
+def cache_request(url, method='GET', data={}, wait=2):
+  time.sleep(wait)
+  if data:
+    response = requests.request(method, url, data=data)
+  else:
+    response = requests.request(method, url)
   return response.text
---
+++ virginia/main.py
@@ -0,0 +1,41 @@
+from common import cache_request
+import requests
+import json
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+import time
+from tqdm import tqdm
+
+def get_locality_ids():
+  page = cache_request('https://vote.elections.virginia.gov/VoterInformation/PublicContactLookup')
+  soup = BeautifulSoup(page, 'lxml')
+  return [option['value'] for option in soup.select('select>option') if option['value']]
+
+def get_locality_daum(id_):
+  page = cache_request(
+    'https://vote.elections.virginia.gov/VoterInformation/PublicContactLookup',
+    method='POST',
+    data={'LocalityUid': id_}
+  )
+  soup = BeautifulSoup(page, 'lxml')
+  keys = soup.select('.resultsWrapper')[0].select('h5.display-lable')
+  vals = soup.select('.resultsWrapper')[0].select('p.display-field')
+  assert(len(keys) == len(vals))
+  results = { key.text.strip(): val.text.strip() for key, val in zip(keys, vals) }
+  final = {
+    'locale': soup.select('select > option[selected="selected"]')[0].text.title(),
+    'official': results['Registrar'],
+    'emails': [results['Email']],
+    'faxes': [results['Fax']],
+    'url': results.get('URL'),
+    'address': results.get('Mailing Address') or results.get('Address'),
+    'physicalAddress': results.get('Physical Address'),
+  }
+  return { k: v for k, v in final.items() if v }
+
+if __name__ == '__main__':
+  ids = get_locality_ids()
+
+  locality_data = [get_locality_daum(id_) for id_ in tqdm(get_locality_ids())]
+  with open('public/virginia.json', 'w') as fh:
+    json.dump(locality_data, fh)
