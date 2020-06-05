b4e5e1ecb40e75e8218f3bb518c7c552d08301a0
Tristan Wagner <twagner000@users.noreply.github.com>
2020-06-04 17:53:42 -0700

    use regex to extract county/phone/mailing sections
    - replaced group_rows with regex and moved into generate_county_dict_list (renamed parse_pdf)
    - used zip to iterate through multiple lists of equal length
    - moved code from parse_html_emails into main since it was pretty short after some refactoring

--- states/oklahoma/main.py
+++ states/oklahoma/main.py
@@ -1,19 +1,33 @@
-import parse_pdf
-import parse_html_emails
 import json
+from bs4 import BeautifulSoup
+
+from common import cache_request, diff_and_save #normalize_state, diff_and_save
+from parse_pdf import parse_pdf
+
+
+EMAIL_LIST_URL = 'https://www.ok.gov/elections/County_Election_Board_Email_Addresses.html'
+
+def parse_email_list():
+  text = cache_request(EMAIL_LIST_URL)
+  soup = BeautifulSoup(text, 'html.parser')
+  email_by_county = {}
+  for row in soup.find('tbody')('tr'):
+    county_name = row.find('td').string
+    email_tag = row.find('a')
+    if email_tag is not None:
+      email_by_county[county_name] = email_tag.string
+  return email_by_county
+
+def crawl_and_parse():
+  pdf_data = parse_pdf()
+  email_data = parse_email_list()
+  assert(len(pdf_data) == len(email_data))
+
+  for county_name, email in email_data.items():
+    pdf_data[county_name]['emails'] = [email]
+
+  data = normalize_state(list(pdf_data.values()))
+  diff_and_save(data, 'public/oklahoma.json')
 
 if __name__ == "__main__":
-    # Get all data except emails
-    counties = parse_pdf.main()
-    # Get all emails.
-    emails = parse_html_emails.main()
-
-    assert len(counties) == len(emails)
-
-    for county_name, email in emails.items():
-        counties[county_name]['emails'].append(email)
-
-    counties_as_list = list(counties.values())
-
-    with open('public/oklahoma.json', 'w') as f:
-        json.dump(counties_as_list, f)
+  crawl_and_parse()
--- states/oklahoma/parse_pdf.py
+++ states/oklahoma/parse_pdf.py
@@ -5,95 +5,48 @@ from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from io import StringIO
-import os
-from collections import defaultdict
+import re
 
 
-def get_text(url):
+PDF_URL = 'https://www.ok.gov/elections/documents/CEB_Physical%20Addresses_%283-19-2020%29.pdf'
+
+re_county_section = re.compile('(?<=COUNTY\n).*?(?=\n\n|\n\*)', flags=re.MULTILINE+re.DOTALL) # edge case where next line starts with *
+re_phone_fax_section = re.compile('(?<=PHONE\n).*?(?=\n\n)', flags=re.MULTILINE+re.DOTALL)
+re_mailing_section = re.compile('(?<=MAILING ADDRESS\n).*?(?=\n\n)', flags=re.MULTILINE+re.DOTALL)
+re_number_space = re.compile('[\d]+\s*')
+
+
+def get_pdf_text(url):
   req = cache_request(url, is_binary=True)
   with BytesIO(req) as fh:
-    output = StringIO()
-    manager = PDFResourceManager()
-    converter = TextConverter(manager, output, laparams=LAParams())
-    interpreter = PDFPageInterpreter(manager, converter)
-
-    for page in PDFPage.get_pages(fh, set()):
-      interpreter.process_page(page)
-    
-    converter.close()
-    text = output.getvalue()
-    output.close()
+    with StringIO() as output:
+      manager = PDFResourceManager()
+      converter = TextConverter(manager, output, laparams=LAParams())
+      interpreter = PDFPageInterpreter(manager, converter)
+      for page in PDFPage.get_pages(fh, set()):
+        interpreter.process_page(page)
+      converter.close()
+      text = output.getvalue()
     return text
 
+def parse_pdf():
+  text = get_pdf_text(PDF_URL)
 
-def group_rows(lines):
-  # There is no email address, and fax and phone share a line.
-  sections = ['COUNTY', 'PHONE', 'MAILING ADDRESS']
+  counties = re_number_space.sub('','\n'.join(re_county_section.findall(text))).split('\n')
+  phones_faxes = '\n'.join(re_phone_fax_section.findall(text)).split('\n')
+  mailing_addrs = '\n'.join(re_mailing_section.findall(text)).split('\n')
+  assert(len(counties) == len(phones_faxes))
+  assert(len(counties) == len(mailing_addrs))
 
-  # Only add data to output if it is between one of the above sections and an empty string.
-  add_data = False
-
-  rows = {}
-  rows['COUNTY'] = []
-  rows['PHONE'] = []
-  rows['MAILING ADDRESS'] = []
-
-  section = ''
-
-  for line in lines:
-    # Edge case where one group lines does not end in empty string.
-    if 'Please call CEB' in line:
-      continue
-
-    if line in sections:
-      add_data = True
-      section = line
-      continue
-    elif line == '':
-      add_data = False
-
-    if add_data:
-      rows[section].append(line)
-
-  assert(len(rows['COUNTY']) == len(rows['PHONE']))
-  assert(len(rows['COUNTY']) == len(rows['MAILING ADDRESS']))
-
-  return rows
-
-
-def generate_county_dict_list(rows):
-  counties = {}
-
-  for i in range(0, len(rows['COUNTY'])):
-    county = defaultdict(list)
-
-    county_name = rows['COUNTY'][i].strip('0123456789 ')
-
-    county['county'] = (county_name + ' County')
-    county['locale'] = county['county']
-
-    phone_number, fax_number = rows['PHONE'][i].split(' ')
-
-    county['phones'].append(phone_number)
-    county['faxes'].append(fax_number)
-
-    county['address'] = rows['MAILING ADDRESS'][i]
-
-    counties[county_name] = county
-
-  return counties
-
-
-def main():
-  text = get_text('https://www.ok.gov/elections/documents/CEB_Physical%20Addresses_%283-19-2020%29.pdf')
-  lines = text.split('\n')
-  # remove header
-  lines = lines[5:]
-  rows = group_rows(lines)
-  counties = generate_county_dict_list(rows)
-
-  return counties
-
-
-if __name__ == '__main__':
-  main()
+  data = {}
+  for county, phone_fax, mailing_addr in zip(counties,phones_faxes,mailing_addrs):
+    county_name = f'{county} County'
+    phone, fax = phone_fax.split(' ')
+    data[county] = {
+      'county': county_name,
+      'locale': county_name,
+      'phones': [phone],
+      'faxes': [fax],
+      'address': mailing_addr,
+      }
+  return data
