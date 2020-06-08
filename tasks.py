021c892691d86cc49a00684a1ac2ca6cb66ff0e2
Tristan Wagner <twagner000@users.noreply.github.com>
2020-06-08 12:21:06 -0700

    add back some codes; separate pylint warning run

--- tasks.py
+++ tasks.py
@@ -1,6 +1,5 @@
 from glob import glob
 import os
-
 from invoke import task
 
 VENV_ACTIVATE = "venv\\Scripts\\activate" if os.name == 'nt' else ". ./venv/bin/activate"
@@ -18,12 +17,13 @@ def collect(c, state):
 
 
 @task
-def lint(c):
+def lint(c, warn=None):
   dirs = [d for d in glob('**/') if not d.startswith('venv')]
   pyfiles = glob('*.py')
+  extra_opt = '--enable=R,C --disable=C0103,C0114,C0115,C0116 --exit-zero --reports=y' if warn else ''
   for d in dirs:
     pyfiles += glob(f'{d}**/*.py', recursive=True)
   if glob('venv/'):
-    c.run(f"{VENV_ACTIVATE} && pylint {' '.join(pyfiles)} --rcfile=.pylintrc")
+    c.run(f"{VENV_ACTIVATE} && pylint {' '.join(pyfiles)} --rcfile=.pylintrc {extra_opt}")
   else:
-    c.run(f"pylint {' '.join(pyfiles)} --rcfile=.pylintrc")
+    c.run(f"pylint {' '.join(pyfiles)} --rcfile=.pylintrc {extra_opt}")
