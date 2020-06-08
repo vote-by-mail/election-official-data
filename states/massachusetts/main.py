7821df150d185c0d8f6813bf204dbfd4a9456952
Tristan Wagner <twagner000@users.noreply.github.com>
2020-06-08 11:22:36 -0700

    use regex for massachusetts
    now passes test_public.py

--- states/massachusetts/main.py
+++ states/massachusetts/main.py
@@ -1,38 +1,41 @@
+import re
 from bs4 import BeautifulSoup
-
 from common import cache_selenium, normalize_state, diff_and_save
-
 
 # js obfuscation requires cache_request_selenium
 BASE_URL = 'https://www.sec.state.ma.us/ele/eleclk/clkidx.htm'
 
+re_city = re.compile(r'^(.*?)\n')
+re_addr = re.compile(r'\n(.*?)\n\s*Phone', flags=re.MULTILINE + re.DOTALL)
+re_phone = re.compile(r'\n\s*Phone:\s*(.*?)\n')
+re_fax = re.compile(r'\n\s*Fax:\s*(.*?)\n')
+re_email = re.compile(r'\n\s*Email:\s*(.*?)\n')
+re_url = re.compile(r'\n\s*Website:\s*(.*?)\n')
 
 
 def parse_html(html):
   soup = BeautifulSoup(html, 'html.parser')
-  raw_clerk_data = [x.text for x in soup.select_one('div', {'id': 'content_third'}).find_all("p")]
-  sanitized_clerk_data = []
+  raw_clerk_data = [x.text.strip() for x in soup.select_one("div", id="content_third")("p")]
+  data = []
 
   for stripped_data in raw_clerk_data:
-    stripped_data = [x.lstrip().rstrip() for x in stripped_data.split('\n')]
-
-    if(len(stripped_data) < 8):  # all clerks data blocks follow the same exact format
+    city = (re_city.findall(stripped_data) or [''])[0].title()
+    if not city:
       continue
+    data.append({
+      'locale': city,
+      'city': city,
+      'address': ', '.join(x.strip().title() for x in re_addr.findall(stripped_data)[0].split('\n')),
+      'phones': re_phone.findall(stripped_data),
+      'faxes': re_fax.findall(stripped_data),
+      'emails': re_email.findall(stripped_data),
+      'url': (re_email.findall(stripped_data) or [None])[0],
+    })
 
-    sanitized_data = {
-      'locale': stripped_data[0].capitalize(),
-      'city': stripped_data[0].capitalize(),
-      'address': " ".join(x.capitalize() for x in " ".join(stripped_data[1:4]).split()),
-      'phones': [stripped_data[4].split()[1]],
-      'faxes': [stripped_data[5].split()[1]],
-      'emails': [stripped_data[6].split()[1]],
-      'url': stripped_data[7].split()[1]
-    }
-    sanitized_clerk_data.append(sanitized_data)
+  return data
 
-  return sanitized_clerk_data
-  
-def crawl_and_parse():
+
+def main():
   html = cache_selenium(BASE_URL)
   data = parse_html(html)
   data = normalize_state(data)
@@ -40,4 +43,4 @@ def crawl_and_parse():
 
 
 if __name__ == "__main__":
-  crawl_and_parse()
+  main()
