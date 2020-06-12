eae6d671190c5a9109f306eb937cd91f1de0a265
Tristan Wagner <twagner000@users.noreply.github.com>
2020-06-12 10:49:27 -0700

    save scrapy output in common cache
    update new_hampshire to use `fetch_data()`

--- src/states/new_hampshire/main.py
+++ src/states/new_hampshire/main.py
@@ -1,17 +1,24 @@
+import os
+import json
 import csv
 import re
 from io import StringIO
 import scrapy
 from scrapy.crawler import CrawlerProcess
-from common import normalize_state, diff_and_save
+from common import work_dir
 
 # We use Scrapy for New Hampshire because it has an .aspx __viewstate
 # which must be generated and sent along with the request
 
+BASE_URL = "https://app.sos.nh.gov/Public/Reports.aspx"
+CACHE_FILE_PATH = os.path.relpath(f"{work_dir}/new_hampshire_raw.jl")
+
+re_email = re.compile(r"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$")
+
 
 class NewHampshireSpider(scrapy.Spider):
   name = 'New Hampshire'
-  start_urls = ["https://app.sos.nh.gov/Public/Reports.aspx"]
+  start_urls = [BASE_URL]
 
   def parse(self, response):
     yield scrapy.FormRequest.from_response(
@@ -27,14 +34,10 @@ class NewHampshireSpider(scrapy.Spider):
     csv_str = StringIO(response.text)
     reader = csv.DictReader(csv_str, delimiter=",")
     clerk_data = []
-
     for row in reader:
       data = self.extract_clerk_data(row)
       if data is not None:
         clerk_data.append(data)
-
-    clerk_data = normalize_state(clerk_data)
-    diff_and_save(clerk_data, 'public/new_hampshire.json')
     return clerk_data
 
   def extract_clerk_data(self, row):
@@ -43,12 +46,11 @@ class NewHampshireSpider(scrapy.Spider):
     if town_or_city is None:
       return None
 
-    email_pattern = re.compile(r"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$")
     clerk_data_entry = {}
     clerk_data_entry["city"] = town_or_city
     clerk_data_entry["address"] = row["Address"].strip()
     clerk_data_entry["locale"] = town_or_city
-    clerk_data_entry["emails"] = [row["E-Mail"]] if email_pattern.match(row["E-Mail"]) else []
+    clerk_data_entry["emails"] = [row["E-Mail"]] if re_email.match(row["E-Mail"]) else []
     clerk_data_entry["phones"] = ["603-" + row["Phone (area code 603)"]] if row["Phone (area code 603)"] else []
     clerk_data_entry["faxes"] = ["603-" + row["Fax"]] if(row["Fax"]) else []
     if row["Clerk"]:
@@ -68,7 +70,20 @@ class NewHampshireSpider(scrapy.Spider):
     return capitalized_city.strip()
 
 
+def fetch_data():
+  if os.path.exists(CACHE_FILE_PATH):
+    print('Using cached data')
+  else:
+    process = CrawlerProcess({
+      'FEED_FORMAT': 'jsonlines',
+      'FEED_URI': CACHE_FILE_PATH,
+    })
+    process.crawl(NewHampshireSpider)
+    process.start()
+  with open(CACHE_FILE_PATH) as cached_file:
+    data = [json.loads(line) for line in cached_file]
+  return data
+
+
 if __name__ == "__main__":
-  process = CrawlerProcess()
-  process.crawl(NewHampshireSpider)
-  process.start()
+  print(fetch_data())
