15fc9fcb323b0f9554bb570f7757d4edbf88ede8
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-07-02 11:18:25 -0700

    tweak for style

--- tasks.py
+++ tasks.py
@@ -8,11 +8,9 @@ VENV_ACTIVATE = "venv\\Scripts\\activate" if os.name == 'nt' else ". ./venv/bin/
 def collect(c, state):  # pylint: disable=invalid-name
   c.run(f"{VENV_ACTIVATE} && cd src && inv collect {state}")
 
-
 @task
 def lint(c):  # pylint: disable=invalid-name
   c.run(f"{VENV_ACTIVATE} && pylint tasks.py src --rcfile=.pylintrc")
-
 
 @task
 def test(c):  # pylint: disable=invalid-name
