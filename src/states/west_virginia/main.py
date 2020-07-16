fc280a84df570c6157764a4e96595514331dd2bf
Tristan Wagner <twagner000@users.noreply.github.com>
2020-07-16 12:41:55 -0700

    fix west_virginia - remove ph/fax extensions, refactor
    refactor into `parse_county` function

--- src/states/west_virginia/main.py
+++ src/states/west_virginia/main.py
@@ -1,44 +1,36 @@
+import re
 from bs4 import BeautifulSoup
 from common import cache_request
 
 
 BASE_URL = 'https://sos.wv.gov/elections/Pages/CountyClerkDirectory.aspx'
 
+re_phone_fax = re.compile(r'\d{3}\D*?\d{3}\D*?\d{4}')
+
+
+def parse_county(row):
+  items = [td.text for td in row('td')]
+
+  # some counties have an empty first element
+  if not items[0].strip():
+    items.pop(0)
+
+  county = items[0] + ' County'
+  return {
+    'county': county,
+    'locale': county,
+    'clerk': items[1],
+    'address': ', '.join(x.strip() for x in items[2].strip().split('\n')),
+    'phones': re_phone_fax.findall(items[3]),
+    'faxes': re_phone_fax.findall(items[4]),
+    'emails': [items[5].strip()],
+  }
+
 
 def fetch_data():
-  text = cache_request(BASE_URL)
-  soup = BeautifulSoup(text, 'html.parser')
-
-  raw_rows = soup.body.find_all('tr')
-
-  counties = []
-  for table_row in raw_rows:
-    if table_row is None:
-      continue
-    county = {}
-
-    items = table_row.find_all('td')
-
-    # Some counties' data is offset one index forward.
-    starting_index = 0
-    if len(items) < 6:
-      continue
-    if len(items) == 6:
-      starting_index = 0
-    else:
-      starting_index = 1
-
-    county['county'] = items[starting_index].text + ' County'
-    county['locale'] = county['county']
-    county['clerk'] = items[starting_index + 1].text
-    county['address'] = items[starting_index + 2].text.replace('\n', ' ').strip(' ')
-    # [:12] removes extension
-    county['phones'] = [items[starting_index + 3].text.replace('(', '').replace(')', '-').replace(' ', '')[:12]]
-    county['faxes'] = [items[starting_index + 4].text.replace('(', '').replace(')', '-').replace(' ', '')]
-    county['emails'] = [items[starting_index + 5].text.replace(' ', '')]
-    counties.append(county)
-
-  return counties
+  html = cache_request(BASE_URL)
+  soup = BeautifulSoup(html, 'html.parser')
+  return [parse_county(row) for row in soup.body.select('tbody tr')]
 
 
 if __name__ == '__main__':
