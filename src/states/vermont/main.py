8e89466e3d926a80d06b499bd06fd0762db51afb
Tristan Wagner <twagner000@users.noreply.github.com>
2020-07-16 10:54:43 -0700

    attempt with `wget` instead of selenium

--- src/states/vermont/main.py
+++ src/states/vermont/main.py
@@ -1,12 +1,12 @@
 import os
 import os.path
-import time
 from ediblepickle import checkpoint
-from selenium import webdriver
 import pandas as pd
+import wget
 from common import key_namer, work_dir, to_list
 
 BASE_URL = 'https://sos.vermont.gov/elections/town-clerks/'
+XLSX_URL = 'https://sos.vermont.gov/media/fyvdm1j0/clerkcontactinfoexcel.xlsx'
 
 
 def vermont_key_namer(args, kwargs):
@@ -16,33 +16,13 @@ def vermont_key_namer(args, kwargs):
 
 
 @checkpoint(key=vermont_key_namer, work_dir=work_dir)
-def cache_xlsx(url=BASE_URL):  # use a keyword arg for key namer
-  """
-  Use Selenium to load the page with the link to the Excel file
-  and click the link to download (since download is protected).
-  Returns page_source (for debugging), contents of xlsx file
-  """
-  options = webdriver.ChromeOptions()
-  options.add_argument('headless')
-  # https://medium.com/@moungpeter/how-to-automate-downloading-files-using-python-selenium-and-headless-chrome-9014f0cdd196
-  options.add_experimental_option("prefs", {
-    "download.default_directory": os.path.abspath(work_dir),
-    "download.prompt_for_download": False,
-    "download.directory_upgrade": True,
-    "safebrowsing_for_trusted_sources_enabled": False,
-    "safebrowsing.enabled": False
-  })
-  with webdriver.Chrome(options=options) as driver:
-    driver.get(url)
-    time.sleep(.5)
-    xlsx_link = driver.find_element_by_partial_link_text('Town Clerk contact information (Excel)')
-    xlsx_link.click()
-    time.sleep(1)  # wait for download
-    xlsx_path = os.path.join(work_dir, xlsx_link.get_attribute("href").split('/')[-1])
-    with open(xlsx_path, mode='rb') as xlsx_file:
-      xlsx = xlsx_file.read()
-    os.remove(xlsx_path)
-    return driver.page_source, xlsx
+def cache_xlsx(url=XLSX_URL):  # use a keyword arg for key namer
+  xlsx_path = wget.download(url, out=work_dir)
+  xlsx_path = os.path.abspath(xlsx_path)
+  with open(xlsx_path, mode='rb') as xlsx_file:
+    xlsx = xlsx_file.read()
+  os.remove(xlsx_path)
+  return xlsx
 
 
 def parse_xlsx(xlsx):
@@ -64,7 +44,7 @@ def parse_xlsx(xlsx):
 
 
 def fetch_data():
-  _, xlsx = cache_xlsx()
+  xlsx = cache_xlsx()
   return parse_xlsx(xlsx)
 
 
