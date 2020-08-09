39fb846062606dae1e47a7989e1e9fe2f9dfb0e7
Tristan Wagner <twagner000@users.noreply.github.com>
2020-08-09 14:31:50 -0700

    florida from cache_request to cache_selenium
    Use updated cache_selenium for multiple sequential requests

--- src/states/florida/main.py
+++ src/states/florida/main.py
@@ -1,31 +1,33 @@
 from bs4 import BeautifulSoup
 from tqdm import tqdm
-
-from common import cache_request, decode_email
+from common import init_selenium_driver, cache_selenium
 
 BASE_URL = 'https://dos.elections.myflorida.com/supervisors/'
 
 
-def parse_county(soup):
+def fetch_and_parse_county(county_url, driver):
+  html = cache_selenium(county_url, wait=1, driver=driver)
+  soup = BeautifulSoup(html, 'html.parser')
   county = soup.find('p', class_='title').text.split('Supervisor')[0].strip()
   links = soup.find(id='rightContent')('a')
   return {
     'locale': county,
     'official': soup.find('span', class_='bigRed').text.replace(u'\xa0', ' ').split(',')[0].strip(),
-    'emails': [decode_email(links[0].find('span', class_='__cf_email__').get('data-cfemail')).strip()],
+    'emails': [links[0]['href'].replace('mailto:', '').strip()],
     'url': links[1]['href'].strip(),
     'county': county,
   }
 
 
 def fetch_data(verbose=True):
-  data = []
-  text = cache_request(BASE_URL)
-  soup = BeautifulSoup(text, 'html.parser')
-  for county_link in tqdm(soup.select('a[href^=countyInfo]'), disable=not verbose):
-    text = cache_request(BASE_URL + county_link['href'])
-    data.append(parse_county(BeautifulSoup(text, 'html.parser')))
-
+  driver = init_selenium_driver()  # will be using repeatedly
+  html = cache_selenium(BASE_URL, driver=driver)
+  soup = BeautifulSoup(html, 'html.parser')
+  county_links = soup.select('a[href^=countyInfo]')
+  assert len(county_links) > 0, ('No county links found in the following HTML:\n' + '#'*30 + html + '#'*30)
+  data = [fetch_and_parse_county(BASE_URL + county_link['href'], driver)
+          for county_link in tqdm(county_links, disable=not verbose)]
+  driver.close()
   return data
 
 
